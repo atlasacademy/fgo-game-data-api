1f0cb7d6ff1d0bc6aabb0651c5a2573755dcfc8f
squaresmile <squaresmile@protonmail.com>
2020-05-19 12:40:48 -0400

    Add traits info

--- app/routers/nice.py
+++ app/routers/nice.py
@@ -4,7 +4,14 @@ from fastapi import APIRouter, HTTPException
 
 from ..data import gamedata
 from ..data.models.common import Region
-from ..data.models.nice import Attribute, CardType, Gender, NiceServantEntity, SvtClass
+from ..data.models.nice import (
+    Attribute,
+    CardType,
+    Gender,
+    NiceServantEntity,
+    SvtClass,
+    Trait,
+)
 
 
 CARD_TYPE_NAME: Dict[int, CardType] = {
@@ -44,6 +51,103 @@ CLASS_NAME: Dict[int, SvtClass] = {
 }
 
 
+TRAIT_NAME: Dict[int, Trait] = {
+    1: Trait.genderMale,
+    2: Trait.genderFemale,
+    3: Trait.genderUnknown,
+    100: Trait.classSaber,
+    101: Trait.classLancer,
+    102: Trait.classArcher,
+    103: Trait.classrider,
+    104: Trait.classCaster,
+    105: Trait.classAssassin,
+    106: Trait.classBerserker,
+    107: Trait.classShielder,
+    108: Trait.classRuler,
+    109: Trait.classAlterEgo,
+    110: Trait.classAvenger,
+    111: Trait.classDemonGodPillar,
+    112: Trait.classGrandCaster,
+    113: Trait.classBeastI,
+    114: Trait.classBeastII,
+    115: Trait.classMoonCancer,
+    116: Trait.classBeastIIIR,
+    117: Trait.classForeigner,
+    118: Trait.classBeastIIIL,
+    119: Trait.classBeastMaybe,
+    200: Trait.attributeSky,
+    201: Trait.attributeEarth,
+    202: Trait.attributeHuman,
+    203: Trait.attributeStar,
+    204: Trait.attributeBeast,
+    300: Trait.alignmentLawful,
+    301: Trait.alignmentChaotic,
+    302: Trait.alignmentNeutral,
+    303: Trait.alignmentGood,
+    304: Trait.alignmentEvil,
+    305: Trait.alignmentBalanced,
+    306: Trait.alignmentMadness,
+    308: Trait.alignmentSummer,
+    1000: Trait.basedOnServant,  # can be NPC or enemy but use a servant's data
+    1001: Trait.human,  # Sanson's 3rd skill
+    1002: Trait.undead,  # Scathach's 3rd skill
+    1003: Trait.artificialDemon,
+    1004: Trait.demonBeast,
+    1005: Trait.daemon,
+    1100: Trait.soldier,
+    1101: Trait.amazoness,
+    1102: Trait.skeleton,
+    1103: Trait.zombie,
+    1104: Trait.ghost,
+    1105: Trait.automata,
+    1106: Trait.golem,
+    1107: Trait.spellBook,
+    1108: Trait.homunculus,
+    1110: Trait.lamia,
+    1111: Trait.centaur,
+    1112: Trait.werebeast,
+    1113: Trait.chimera,
+    1117: Trait.wyvern,
+    1118: Trait.dragonType,
+    1119: Trait.gazer,
+    1120: Trait.handOrDoor,
+    1121: Trait.demonGodPillar,
+    1132: Trait.oni,
+    1133: Trait.hand,
+    1134: Trait.door,
+    1172: Trait.threatToHumanity,
+    2000: Trait.divine,
+    2001: Trait.humanoid,
+    2002: Trait.dragon,
+    2003: Trait.dragonSlayer,
+    2004: Trait.roman,
+    2005: Trait.wildbeast,
+    2006: Trait.atalante,
+    2007: Trait.saberface,
+    2008: Trait.weakToEnumaElish,
+    2009: Trait.riding,
+    2010: Trait.arthur,
+    2011: Trait.skyOrEarth,  # Tesla's NP
+    2012: Trait.brynhildsBeloved,
+    2018: Trait.undeadOrDaemon,  # Amakusa bond Ce
+    2019: Trait.demonic,
+    2037: Trait.skyOrEarthExceptPseudoAndDemi,  # Raikou's 3rd skill
+    2040: Trait.divineOrDaemonOrUndead,  # Ruler Martha's 3rd skill
+    2075: Trait.saberClassServant,  # MHXA NP
+    2076: Trait.superGiant,
+    2113: Trait.king,
+    2114: Trait.greekMythologyMales,
+    2355: Trait.illya,
+    2356: Trait.genderUnknownServant,  # Teach's 3rd skill
+    2466: Trait.argonaut,
+    2615: Trait.genderCaenisServant,  # Phantom's 2nd skill
+    2631: Trait.humanoidServant,  # used in TamaVitch's fight
+    2632: Trait.beastServant,  # used in TamaVitch's fight
+    5000: Trait.canBeInBattle,  # can be NPC, enemy or playable servant i.e. not CE
+    5010: Trait.notBasedOnServant,
+}
+
+
 def get_nice_servant(region: Region, item_id: int) -> Dict[str, Any]:
     raw_data = gamedata.get_servant_entity(region, item_id)
     nice_data: Dict[str, Any] = {}
@@ -57,6 +161,11 @@ def get_nice_servant(region: Region, item_id: int) -> Dict[str, Any]:
     nice_data["instantDeathChance"] = raw_data.mstSvt.deathRate / 1000
     nice_data["starAbsorb"] = raw_data.mstSvtLimit[0].criticalWeight
     nice_data["starGen"] = raw_data.mstSvt.starRate / 1000
+    nice_data["traits"] = [
+        TRAIT_NAME.get(item, Trait.unknown)
+        for item in sorted(raw_data.mstSvt.individuality)
+        if item != item_id
+    ]
 
     atkMax = raw_data.mstSvtLimit[0].atkMax
     atkBase = raw_data.mstSvtLimit[0].atkBase
