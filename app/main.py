acd77adc7d3fc457d5dc66d6211439cd8f8b036f
squaresmile <squaresmile@protonmail.com>
2020-05-31 22:49:10 -0400

    Bump version to 0.2.0

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ if settings.documentation_all_nice:
     app_description += export_links
 
 
-app = FastAPI(title="FGO Game data API", description=app_description, version="0.1.0")
+app = FastAPI(title="FGO Game data API", description=app_description, version="0.2.0")
 
 
 app.add_middleware(
