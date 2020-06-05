44e06ea5c32d7f731d01a31234f0f9848b8e87b4
squaresmile <squaresmile@protonmail.com>
2020-06-05 06:16:38 -0400

    Use modified fuzzywuzzy.token_set_ratio to solve the Okita Alter case

--- app/data/gamedata.py
+++ app/data/gamedata.py
@@ -3,7 +3,7 @@ import time
 from copy import deepcopy
 from typing import Any, Dict, List, Set
 
-from fuzzywuzzy import fuzz
+from fuzzywuzzy import fuzz, utils
 
 import orjson
 
@@ -318,12 +318,50 @@ def get_servant_entity(
     return svt_entity
 
 
-def match_name(search_param: str, name: str) -> Any:
+def match_name(search_param: str, name: str) -> bool:
+    """Modified from fuzzywuzzy.token_set_ratio"""
+    p1 = utils.full_process(search_param, force_ascii=True)
+    p2 = utils.full_process(name, force_ascii=True)
+
+    if not utils.validate_string(p1):
+        return False
+    if not utils.validate_string(p2):
+        return False
+
+    # Doesn't seem to be needed now but might be useful in the future
+    # ALTERNATIVE_DIVIDER = ["-", "="]
+    # for divider in ALTERNATIVE_DIVIDER:
+    #     p1 = p1.replace(divider, " ")
+    #     p2 = p2.replace(divider, " ")
+
+    # pull tokens
+    tokens1 = set(p1.split())
+    tokens2 = set(p2.split())
+
+    intersection = tokens1.intersection(tokens2)
+    diff1to2 = tokens1.difference(tokens2)
+    diff2to1 = tokens2.difference(tokens1)
+
+    sorted_sect = " ".join(sorted(intersection))
+    sorted_1to2 = " ".join(sorted(diff1to2))
+    sorted_2to1 = " ".join(sorted(diff2to1))
+
+    combined_1to2 = sorted_sect + " " + sorted_1to2
+    combined_2to1 = sorted_sect + " " + sorted_2to1
+
+    # strip
+    sorted_sect = sorted_sect.strip()
+    combined_1to2 = combined_1to2.strip()
+    combined_2to1 = combined_2to1.strip()
+
     NAME_MATCH_THRESHOLD = 80
-    return (
-        fuzz.token_set_ratio(search_param, name, force_ascii=False)
-        > NAME_MATCH_THRESHOLD
-    )
+
+    # Use sorted_sect first so "Okita Souji (Alter)" works as expected
+    # This way "a b c" search_param will match to "a b c d e" but not vice versa
+    if sorted_sect:
+        return fuzz.ratio(sorted_sect, combined_1to2) > NAME_MATCH_THRESHOLD  # type: ignore
+    else:
+        return fuzz.ratio(combined_2to1, combined_1to2) > NAME_MATCH_THRESHOLD  # type: ignore
 
 
 def search_servant(search_param: ServantSearchQueryParams) -> List[int]:
---
+++ tests/test_search.py
@@ -0,0 +1,116 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+class TestEquipSearch:
+    def test_search_name(self):
+        response = client.get("/nice/NA/equip/search?name=Kaleidoscope")
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {9400340}
+
+    def test_search_name_raw(self):
+        response = client.get("/raw/JP/equip/search?name=カレイドスコープ")
+        assert response.status_code == 200
+        assert {item["mstSvt"]["id"] for item in response.json()} == {9400340}
+
+    def test_search_name_rarity(self):
+        response = client.get("/nice/NA/equip/search?name=Kaleidoscope&rarity=4")
+        assert response.status_code == 200
+        assert response.text == "[]"
+
+    def test_search_names(self):
+        response = client.get("/nice/NA/equip/search?name=Banquet")
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {9302550, 9400290}
+
+    def test_NA_search_no_query(self):
+        response = client.get("/nice/NA/equip/search")
+        assert response.status_code == 400
+
+    def test_JP_search_no_query(self):
+        response = client.get("/raw/JP/equip/search")
+        assert response.status_code == 400
+
+
+class TestServantSearch:
+    def test_search_name(self):
+        response = client.get("/raw/NA/servant/search?name=Pendragon")
+        assert response.status_code == 200
+        assert {item["mstSvt"]["id"] for item in response.json()} == {
+            100100,
+            100200,
+            100300,
+            102900,
+            202600,
+            301900,
+            302000,
+            402200,
+            402700,
+        }
+
+    def test_search_name_rarity_class(self):
+        response = client.get(
+            "/nice/NA/servant/search?name=Pendragon&rarity=5&className=saber"
+        )
+        print({item["id"] for item in response.json()})
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {100100, 102900}
+
+    def test_search_name_rarity_class_gender(self):
+        response = client.get(
+            "/nice/NA/servant/search?name=Pendragon&rarity=5&className=saber&gender=female"
+        )
+        print({item["id"] for item in response.json()})
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {100100}
+
+    def test_search_name_class_attribute(self):
+        response = client.get("/nice/NA/servant/search?className=archer&attribute=star")
+        print({item["id"] for item in response.json()})
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {201100, 202200}
+
+    def test_search_name_class_trait_rarity(self):
+        response = client.get(
+            "/nice/JP/servant/search?className=rider&trait=king&lang=en&rarity=3"
+        )
+        print({item["id"] for item in response.json()})
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {401100, 401500, 403900}
+
+    def test_nice_search_no_query(self):
+        response = client.get("/nice/NA/servant/search")
+        assert response.status_code == 400
+
+    def test_raw_search_no_query(self):
+        response = client.get("/raw/NA/servant/search")
+        assert response.status_code == 400
+
+    def test_NA_search_fuzzy_process_empty(self):
+        response = client.get("/raw/NA/servant/search?name=ÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛ")
+        assert response.status_code == 200
+        assert response.text == "[]"
+
+    def test_NA_search_Okita_Alter(self):  # shouldn't return Okita Saber
+        response = client.get("/raw/NA/servant/search?name=Okita Souji (Alter)")
+        assert response.status_code == 200
+        assert {item["mstSvt"]["id"] for item in response.json()} == {1000700}
+
+    def test_JP_search_Skadi(self):
+        response = client.get("/raw/JP/servant/search?name=Skadi")
+        assert response.status_code == 200
+        assert {item["mstSvt"]["id"] for item in response.json()} == {503900}
+
+    def test_NA_search_Scathach(self):
+        response = client.get("/nice/NA/servant/search?name=Scathach")
+        assert response.status_code == 200
+        assert {item["id"] for item in response.json()} == {301300, 602400}
+
+    def test_NA_search_Yagyu(self):
+        response = client.get("/raw/NA/servant/search?name=Tajima")
+        assert response.status_code == 200
+        assert {item["mstSvt"]["id"] for item in response.json()} == {103200}
