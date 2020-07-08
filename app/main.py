7dbce51d9ff84e698abeb8dc23a2580e2ebd12dd
squaresmile <squaresmile@protonmail.com>
2020-07-07 23:12:44 -0400

    Bump to version 0.11.0

--- app/main.py
+++ app/main.py
@@ -67,7 +67,7 @@ tags_metadata = [
 app = FastAPI(
     title="FGO game data API",
     description=app_description,
-    version="0.10.0",
+    version="0.11.0",
     docs_url=None,
     openapi_tags=tags_metadata,
 )
