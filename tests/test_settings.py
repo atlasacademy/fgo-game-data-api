9d46474deb19a78162f7500d56db00e171acc5b9
squaresmile <squaresmile@protonmail.com>
2020-07-31 06:58:16 -0400

    Added type hint for monkeypatch

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,13 +1,15 @@
+from _pytest.monkeypatch import MonkeyPatch
+
 from app.config import Settings
 
 
 class TestSettings:
-    def test_url_trim_slash(self, monkeypatch) -> None:
+    def test_url_trim_slash(self, monkeypatch: MonkeyPatch) -> None:
         monkeypatch.setenv("ASSET_URL", "https://example.com/assets/")
         settings = Settings()
         assert settings.asset_url == "https://example.com/assets"
 
-    def test_url_trim_slash_no_shash(self, monkeypatch) -> None:
+    def test_url_trim_slash_no_shash(self, monkeypatch: MonkeyPatch) -> None:
         monkeypatch.setenv("ASSET_URL", "https://example.com/assets")
         settings = Settings()
         assert settings.asset_url == "https://example.com/assets"
