e8b6bbc4838be72ea0ba93aa4f9722298d9d8914
squaresmile <squaresmile@protonmail.com>
2021-02-09 19:43:44 -0500

    Always create db tables even if the json files don't exist

--- app/db/load.py
+++ app/db/load.py
@@ -22,35 +22,41 @@ def recreate_table(engine: Engine, table: Table) -> None:  # pragma: no cover
 def update_db(region_path: Dict[Region, DirectoryPath]) -> None:  # pragma: no cover
     logger.info("Loading db …")
     start_loading_time = time.perf_counter()
-    for region_name, gamedata in region_path.items():
-        engine = engines[region_name]
+
+    for region, master_folder in region_path.items():
+        engine = engines[region]
 
         with engine.connect() as conn:
             for table in TABLES_TO_BE_LOADED:
-                table_json = gamedata / f"{table.name}.json"
+                recreate_table(engine, table)
+
+                table_json = master_folder / f"{table.name}.json"
                 if table_json.exists():
                     with open(table_json, "rb") as fp:
                         data = orjson.loads(fp.read())
-                    recreate_table(engine, table)
                     conn.execute(table.insert(data))
+                else:
+                    logger.warning(f"Can't find file {table_json}.")
 
-                    if table is mstSvtVoice:
-                        conn.execute(text('DROP INDEX IF EXISTS "mstSvtVoiceGIN"'))
-                        conn.execute(
-                            text(
-                                'CREATE INDEX "mstSvtVoiceGIN" ON "mstSvtVoice" '
-                                'USING gin("scriptJson" jsonb_path_ops)'
-                            )
+                if table is mstSvtVoice:
+                    conn.execute(text('DROP INDEX IF EXISTS "mstSvtVoiceGIN"'))
+                    conn.execute(
+                        text(
+                            'CREATE INDEX "mstSvtVoiceGIN" ON "mstSvtVoice" '
+                            'USING gin("scriptJson" jsonb_path_ops)'
                         )
+                    )
 
             recreate_table(engine, mstSubtitle)
-            subtitle_json = gamedata / "globalNewMstSubtitle.json"
+            subtitle_json = master_folder / "globalNewMstSubtitle.json"
             if subtitle_json.exists():
                 with open(subtitle_json, "rb") as fp:
                     globalNewMstSubtitle = orjson.loads(fp.read())
                 for subtitle in globalNewMstSubtitle:
                     subtitle["svtId"] = get_subtitle_svtId(subtitle["id"])
                 conn.execute(mstSubtitle.insert(globalNewMstSubtitle))
+            elif region == Region.NA:
+                logger.warning(f"Can't find file {subtitle_json}.")
 
     db_loading_time = time.perf_counter() - start_loading_time
     logger.info(f"Loaded db in {db_loading_time:.2f}s.")
