fe04292e1302e2656d173cb01cff2e0bcce26a39
squaresmile <squaresmile@protonmail.com>
2022-03-16 09:04:36 +0700

    Added support for globs in json format script

--- scripts/format_json.py
+++ scripts/format_json.py
@@ -8,9 +8,9 @@ def format_file(path: Path) -> None:
     print(f"Formatting {path.name} ... ", end="")
     with open(path, "rb") as f:
         content = orjson.loads(f.read())
+    orjson_opts = orjson.OPT_INDENT_2 | orjson.OPT_APPEND_NEWLINE
     with open(path, "wb") as f:
-        f.write(orjson.dumps(content, option=orjson.OPT_INDENT_2))
-        f.write(b"\n")
+        f.write(orjson.dumps(content, option=orjson_opts))
     print("Done")
 
 
@@ -23,6 +23,16 @@ def main(input_folder: list[str]) -> None:
                     format_file(file)
         elif resolved_path.is_file():
             format_file(resolved_path)
+        else:
+            for file in Path().glob(path):
+                if (
+                    file.is_file()
+                    and file.suffix == ".json"
+                    and not (
+                        file.name.startswith("nice_") or file.name.startswith("basic_")
+                    )
+                ):
+                    format_file(file)
 
 
 if __name__ == "__main__":
