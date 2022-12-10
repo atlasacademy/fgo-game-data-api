85d1dfb93e9e253a1d2dd9007d1568b17e995c01
squaresmile <squaresmile@protonmail.com>
2022-12-10 18:33:51 +0700

    Updated translations and packages

--- scripts/dump_rayshiftQuest.ps1
+++ scripts/dump_rayshiftQuest.ps1
@@ -1,8 +1,8 @@
 Remove-Item rayshift_quest_jp.sql
 Remove-Item rayshift_quest_na.sql
 Remove-Item rayshiftQuest.zip
-docker-compose exec -it pg-fgoapijp pg_dump --username=user --password --if-exists --clean --table='\"rayshiftQuest\"' --file=rayshift_quest_jp.sql fgoapijp
-docker-compose exec -it pg-fgoapina pg_dump --username=user --password --if-exists --clean --table='\"rayshiftQuest\"' --file=rayshift_quest_na.sql fgoapina
+docker-compose exec -it pg-fgoapijp pg_dump --username=user --password --if-exists --clean --table='"rayshiftQuest"' --file=rayshift_quest_jp.sql fgoapijp
+docker-compose exec -it pg-fgoapina pg_dump --username=user --password --if-exists --clean --table='"rayshiftQuest"' --file=rayshift_quest_na.sql fgoapina
 docker-compose cp pg-fgoapijp:rayshift_quest_jp.sql ./
 docker-compose cp pg-fgoapina:rayshift_quest_na.sql ./
 $compress = @{
