86699d55da1f42d931b59a2f63fcd90e248c3d19
squaresmile <squaresmile@protonmail.com>
2023-02-25 07:07:24 +0700

    Updated buff list action list

--- scripts/format.ps1
+++ scripts/format.ps1
@@ -1,4 +1,4 @@
 isort app tests export scripts
 black app tests export scripts
 
-python ./scripts/format_json.py ./app/data/mappings ./tests/ ./export/*/Nice*.json
+python ./scripts/format_json.py ./app/data/mappings ./tests/ ./export/*/Nice*.json ./export/BuffList.ActionList.json
