eb6410cd4bd85ec61d3b68f64a93cb28bca00dba
narumi <yan.jianhui147@gmail.com>
2024-04-22 01:18:14 +0800

    Fixed SHORTEN_SKILL func dataVal parsing

--- app/core/nice/func.py
+++ app/core/nice/func.py
@@ -131,6 +131,8 @@ async def parse_dataVals(
                     FuncType.SUB_STATE,
                     FuncType.GAIN_NP_INDIVIDUAL_SUM,
                     FuncType.SUB_FIELD_BUFF,
+                    FuncType.SHORTEN_SKILL,
+                    FuncType.EXTEND_SKILL,
                     FuncType.SHORTEN_USER_EQUIP_SKILL,
                     FuncType.EXTEND_USER_EQUIP_SKILL,
                 }:
