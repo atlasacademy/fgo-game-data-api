c047fdc1006f5bbdde57816f7ac4f2099173fa25
squaresmile <squaresmile@protonmail.com>
2024-04-30 08:34:59 -0400

    Always include spawn bonus enemy in quest response

--- app/core/nice/quest.py
+++ app/core/nice/quest.py
@@ -674,7 +674,7 @@ async def get_nice_quest_phase(
                         rayshift_quest_details[0],
                         rayshift_quest_drops,
                         lang,
-                        include_spawn_bonus_enemy=questHash is not None,
+                        include_spawn_bonus_enemy=True,
                     )
                 nice_quest_drops = [
                     get_nice_drop(drop)
