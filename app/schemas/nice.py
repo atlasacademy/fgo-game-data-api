2d1aaec3c0f58f9892ce57cfa8eb3213e2f9c257
squaresmile <squaresmile@protonmail.com>
2024-12-23 11:44:29 +0700

    Added new datavals

--- app/schemas/nice.py
+++ app/schemas/nice.py
@@ -613,6 +613,10 @@ class BaseVals(BaseModel):
     EnemyCountChangeTime: int | None = None
     EnemyCountChangeEffectId: int | None = None
     EnemyCountWaitTimeAfterMessage: int | None = None
+    WaitMessageEnd: int | None = None
+    ContinueDisplayMessage: int | None = None
+    StartIntervalTurn: int | None = None
+    StartIntervalCount: int | None = None
     # Extra dataval from SkillLvEntty.DIC_KEY_APPLY_SUPPORT_SVT
     ApplySupportSvt: Optional[int] = None
     # These are not DataVals but guesses from SkillLvEntity and EventDropUpValInfo
