963de069f4bb2655c7ab68ad489996f4e473dc88
narumi <56487872+narumi147@users.noreply.github.com>
2025-12-04 00:28:54 +0800

    Fix closed war banner link

--- app/core/nice/war.py
+++ app/core/nice/war.py
@@ -352,9 +352,9 @@ async def get_nice_war(
 
         if region == Region.JP:
             banner_id = raw_war.mstWar.bannerId
-            if (
-                raw_war.mstWar.id < 10000
-                and raw_war.mstWar.flag & WarEntityFlag.NOTICE_BOARD != 0
+            if raw_war.mstWar.id < 10000 and (
+                raw_war.mstWar.flag & WarEntityFlag.NOTICE_BOARD != 0
+                or (last_war_id and raw_war.mstWar.id <= last_war_id.value)
             ):
                 banner_template = "questboard_cap_closed_{:>03}"
             else:
